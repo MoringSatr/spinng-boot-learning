287980bdc2482ec80763812be53fb2f9af7f6e18
liubowen <liubowen>
2018-04-19 15:50:10 +0800

    commit

---
+++ learning-02/RADEME.md
@@ -0,0 +1,1 @@
+快捷搭建 REST服务框架 + jdbc template
---
+++ learning-02/src/test/java/top/liubowen/learning02/ApplicationTests.java
@@ -0,0 +1,14 @@
+package top.liubowen.learning02;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class ApplicationTests {
+
+
+}
